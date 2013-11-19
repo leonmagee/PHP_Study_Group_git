93f3fc84f9638f51a1b9e86e9499d72c3a726db6
Leon Magee <leonmagee@hotmail.com>
2013-11-18 19:52:58 -0800

    fixed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 echo "php is working!"; 
 
-if (isset($_GE['name']) && isset($_GET['lastname']))) {
+if (isset($_GET['name']) && isset($_GET['lastname']))) {
    echo "Hello {$_GET['name']} {$_GET['lastname]}!";
 } else {
    echo "Hello Random person!";
