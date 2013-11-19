c2616a843b6ea04fc787da1a0a0dfae032523e90
Leon Magee <leonmagee@hotmail.com>
2013-11-18 19:49:42 -0800

    second name added

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 echo "php is working!"; 
 
-if (isset($_GE['name'])) {
-   echo "Hello $_GET['name']!";
+if (isset($_GE['name']) && isset($_GET['lastname']))) {
+   echo "Hello {$_GET['name']} {$_GET['lastname]}!";
 } else {
    echo "Hello Random person!";
 }
